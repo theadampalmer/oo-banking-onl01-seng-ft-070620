4d433ef899a9fe6a4a6f9ea4a30baead607af455
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 22:25:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,16 @@
 class BankAccount
-
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = open
+  end
+  
+  def deposit
+  end
+  
+  def display_balance
+  end
+  
+  
 end
