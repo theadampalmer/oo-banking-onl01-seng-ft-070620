f897858b4357f7323fe12c3e36f5185dc05cf625
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 22:56:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,8 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :balance, :status
   
-  def initialize(name)
+  def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = balance
     @status = status
