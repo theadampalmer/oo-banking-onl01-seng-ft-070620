ea1be04b5e53194e07d9b141d7e45ff50153d8d7
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 23:12:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
+      self.status = "complete"
     else
       reject_transfer
     end
