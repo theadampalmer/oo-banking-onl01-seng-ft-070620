2f65f887b6b86dd1f546c0c7abc8683399f2a298
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 22:36:41 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,28 +1,28 @@
-# class BankAccount
+class BankAccount
   
-#   def initialize(name)
-#     @name = name
-#     @balance = 1000
-#     @status = "open"
-#   end
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
   
-#   def deposit(amount)
-#     @balance += amount
-#   end
+  def deposit(amount)
+    @balance += amount
+  end
   
-#   def display_balance
-#     "Your balance is $#{@balance}."
-#   end
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
   
-#   def valid?
-#     if (@status == "open" && @balance > 0)
-#       true
-#     else
-#       false
-#     end
+  def valid?
+    if (@status == "open" && @balance > 0)
+      true
+    else
+      false
+    end
     
-#   def close_account
-#     @status = "closed"
-#   end
+  def close_account
+    @status = "closed"
+  end
   
-# end
+end
