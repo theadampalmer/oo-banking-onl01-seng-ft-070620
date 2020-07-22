2a1cd23057a5bdad72984dfcf30e380b5a0d83d4
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 23:21:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.balance > amount && status == "pending"
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
