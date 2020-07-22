edbae813f98526cc92dc7a4f6287f18654252238
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 22:36:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 class Transfer
+  
+  def initialize
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
 
+  def valid?
+    
+  end
+  
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
 end
