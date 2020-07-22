f2e843855e00d0da82b4a22b1fa6f3ea5c4543c5
Adam Palmer <theadampalmer@gmail.com>
2020-07-22 22:48:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -10,14 +10,32 @@ class Transfer
   end
 
   def valid?
-    
+      if (sender.valid? && receiver.valid?)
+        true
+      else
+        false
+      end
   end
-  
+
   def execute_transaction
-    
+    if @status == "complete"
+
+    elsif (@receiver.status == "closed" || @sender.balance - amount < 0)
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    else
+      @sender.balance -= @amount
+      @receiver.deposit(@amount)
+      @status = "complete"
+    end
   end
-  
+
   def reverse_transfer
-    
+    if @status == "complete"
+      @receiver.balance -= @amount
+      @sender.deposit(@amount)
+      @status = "reversed"
+    end
   end
+
 end
